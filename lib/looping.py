622365edb3860c2da1c10bfce927a381ff0e7e44
egalosman <ahmedmoha1500@gmail.com>
2024-09-23 16:25:29 +0300

    successful

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i >= 1:
+        print(i)
+        i -= 1
+        
+    print("Happy New Year!")
+
+
+
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    new_squares = [x*x for x in int_list]  
+    return new_squares
+
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+   
+    for x in range(1, 101):
+        if x % 15 == 0:
+            print("FizzBuzz")
+        elif x % 3 == 0:
+            print("Fizz")
+        elif x % 5 == 0:
+            print("Buzz")
+        else:
+            print(x)
+
+   
+
+
